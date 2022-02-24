37e81caebcf664f84f0909b2c44d82666614d6ef
Juliano Marques <juliano.lima.marques@outlook.com>
2022-02-24 09:12:44 -0300

    adding pokemon project

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,4 +1,5 @@
 import todoimg from "../img/projImages/todoimg.jpg";
+import pokemonimg from "../img/projImages/pokemonimg.png";
 
 const projects = [
   {
@@ -7,6 +8,14 @@ const projects = [
     image: todoimg,
     link1: "https://github.com/juulmrqs/To-Do-List-Project",
     title: "To do list",
+    text: "Created with React",
+  },
+  {
+    id: 2,
+    category: "React",
+    image: pokemonimg,
+    link1: "https://github.com/juulmrqs/Using-Pokemon-API",
+    title: "Pokedex consuming API",
     text: "Created with React",
   },
 ];
